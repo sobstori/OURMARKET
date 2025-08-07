45338ef53ec507631b5ef4883c4c3d323d41814b
sobstori <gomezvc849@gmail.com>
2025-08-07 12:41:02 -0500

    Created mvc and organized files

---
+++ OurMarketBackend/models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OurMarketBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
